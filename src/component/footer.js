6e92105cdfdb07b99d8041e1c9bc347ccae14d1d
stmeem <stmeem@yahoo.com>
2021-10-09 09:45:49 +0600

    modified footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 export const Footer = ()=>{
     return(
         <div className="h-12 bg-blue-500 text-center text-white py-3">
-         Copyright &copy; Sumaiya Meem <span role="img" aria-label="red-heart">❤️</span>
+         Copyright &copy; stmeem <span role="img" aria-label=""></span>
         
       </div>
     )
